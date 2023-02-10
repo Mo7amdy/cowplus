274fc80fff57a725b9092b4e43fd0e0efecd5404
Mo7amdy <pr.mhamdy@gmail.com>
2023-02-10 20:56:33 +0200

    ffffffff

--- Api.py
+++ Api.py
@@ -1,10 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
+import flask
+app = flask.Flask(__name__)
 
-
-@app.route("/")
-def hello():
-    return "Hello, SmartNinja!"
+@app.route("/home")
+def home():
+    return "API get running"
 
 if __name__ == "__main__":
     app.run()
